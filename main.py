68a7e56bbd5ec40709b61d913bbec9e15d7a3d05
a1shuvo <a1shuvosaha@gmail.com>
2021-12-09 22:56:59 +0600

    Send an Email if today is the birthday

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
-import smtplib
-import datetime as dt
+from datetime import datetime
+import pandas as pd
 import random
+import smtplib
 
-my_email = "moulik.it@yahoo.com"
-password = "hinncrzrbomzagkd"
+MY_EMAIL = "moulik.it@yahoo.com"
+MY_PASSWORD = "hinncrzrbomzagkd"
 
-now = dt.datetime.now()
-weekday = now.weekday()
+today = datetime.now()
+today_tuple = (today.month, today.day)
 
-if weekday == 3:
-    with open("quotes.txt", "r") as quote_file:
-        all_quotes = quote_file.readlines()
-        quote = random.choice(all_quotes)
+data = pd.read_csv("birthdays.csv")
+birthdays_dict = {(data_row.month, data_row.day): data_row for (index, data_row) in data.iterrows()}
+
+if today_tuple in birthdays_dict:
+    birthday_person = birthdays_dict[today_tuple]
+    print(birthday_person["email"])
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
 
     with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
         connection.starttls()
-        connection.login(user=my_email, password=password)
-        connection.sendmail(from_addr=my_email,
-                            to_addrs="moulikittest@gmail.com",
-                            msg=f"Subject:Monday Motivation Quotes\n\n{quote}"
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL,
+                            to_addrs=birthday_person["email"],
+                            msg=f"Subject:Happy Birthday To You\n\n{contents}"
                             )
