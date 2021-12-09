756f0fe7e3e6c0e60b2a1a2d2ce5638bcbebc413
a1shuvo <a1shuvosaha@gmail.com>
2021-12-09 20:14:55 +0600

    Send Email A Random Quote On Thursday

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import smtplib
+import datetime as dt
+import random
 
 my_email = "moulik.it@yahoo.com"
 password = "hinncrzrbomzagkd"
 
-with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(from_addr=my_email,
-                        to_addrs="moulikittest@gmail.com",
-                        msg="Subject:Hello\n\nThis is the message body of the email."
-                        )
+now = dt.datetime.now()
+weekday = now.weekday()
+
+if weekday == 3:
+    with open("quotes.txt", "r") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+
+    with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs="moulikittest@gmail.com",
+                            msg=f"Subject:Monday Motivation Quotes\n\n{quote}"
+                            )
